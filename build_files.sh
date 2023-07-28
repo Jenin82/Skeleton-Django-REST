a937f4d4bc8e465edb8f920e1e32025029a593ea
Jenin82 <jenin8282@gmail.com>
2023-07-28 11:05:02 +0530

    test1

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.10 manage.py migrate 
+python3.10 manage.py collectstatic
